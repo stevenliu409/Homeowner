42fe791f78bf1ce1128834c6492ffb7889a661cf
Steven Liu <steven.github@gmail.com>
2016-06-14 20:27:46 -0700

    added details view controller

---
+++ HomePwner/BNRDetailsViewController.h
@@ -0,0 +1,13 @@
+//
+//  BNRDetailsViewController.h
+//  HomePwner
+//
+//  Created by Steven Liu on 2016-06-14.
+//  Copyright © 2016 Steven Liu. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface BNRDetailsViewController : UIViewController
+
+@end
---
+++ HomePwner/BNRDetailsViewController.m
@@ -0,0 +1,21 @@
+//
+//  BNRDetailsViewController.m
+//  HomePwner
+//
+//  Created by Steven Liu on 2016-06-14.
+//  Copyright © 2016 Steven Liu. All rights reserved.
+//
+
+#import "BNRDetailsViewController.h"
+
+@interface BNRDetailsViewController ()
+@property (weak, nonatomic) IBOutlet UITextField *nameField;
+@property (weak, nonatomic) IBOutlet UITextField *serialNumberField;
+@property (weak, nonatomic) IBOutlet UITextField *valueField;
+@property (weak, nonatomic) IBOutlet UILabel *dateLabel;
+
+@end
+
+@implementation BNRDetailsViewController
+
+@end
