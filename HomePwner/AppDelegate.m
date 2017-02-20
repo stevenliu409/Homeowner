2debe8b0b6aea1f046dc9f9f2829a4d52f54f396
Steven Liu <steven.github@gmail.com>
2017-02-20 16:37:25 -0500

    BNR-18 Extras
    - Added Methods to see the application transition states

--- HomePwner/AppDelegate.m
+++ HomePwner/AppDelegate.m
@@ -20,6 +20,7 @@
 - (BOOL)application:(UIApplication *)application didFinishLaunchingWithOptions:(NSDictionary *)launchOptions {
     // Override point for customization after application launch.
     
+    NSLog(@"%@", NSStringFromSelector(_cmd));
     
     BNRItemsViewController *itemVC = [[BNRItemsViewController alloc] init];
     UINavigationController *navVc = [[UINavigationController alloc] initWithRootViewController:itemVC];
@@ -31,6 +32,8 @@
 - (void)applicationWillResignActive:(UIApplication *)application {
     // Sent when the application is about to move from active to inactive state. This can occur for certain types of temporary interruptions (such as an incoming phone call or SMS message) or when the user quits the application and it begins the transition to the background state.
     // Use this method to pause ongoing tasks, disable timers, and throttle down OpenGL ES frame rates. Games should use this method to pause the game.
+    NSLog(@"%@", NSStringFromSelector(_cmd));
+
 }
 
 - (void)applicationDidEnterBackground:(UIApplication *)application {
@@ -46,18 +49,26 @@
     {
         NSLog(@"Could not save any of the BNRItems");
     }
+    NSLog(@"%@", NSStringFromSelector(_cmd));
+
 }
 
 - (void)applicationWillEnterForeground:(UIApplication *)application {
     // Called as part of the transition from the background to the inactive state; here you can undo many of the changes made on entering the background.
+    NSLog(@"%@", NSStringFromSelector(_cmd));
+
 }
 
 - (void)applicationDidBecomeActive:(UIApplication *)application {
     // Restart any tasks that were paused (or not yet started) while the application was inactive. If the application was previously in the background, optionally refresh the user interface.
+    NSLog(@"%@", NSStringFromSelector(_cmd));
+
 }
 
 - (void)applicationWillTerminate:(UIApplication *)application {
     // Called when the application is about to terminate. Save data if appropriate. See also applicationDidEnterBackground:.
+    NSLog(@"%@", NSStringFromSelector(_cmd));
+
 }
 
 @end
